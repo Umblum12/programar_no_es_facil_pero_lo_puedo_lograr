ed091cc0318dd4f13001eccb4747381a5d56c2f7
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-31 04:28:36 -0600

    Agregar archivos de proyecto.

---
+++ programar_no_es_facil_pero_lo_puedo_lograr/Program.cs
@@ -0,0 +1,5 @@
+﻿string ejemplo = "Programar no es fácil, pero lo puedo lograr";
+string ejemplomod = ejemplo.Insert(43, ", atentamente Abelardo");
+Console.WriteLine("{0}", ejemplo);
+Console.WriteLine("{0}", ejemplomod.Replace(", pero lo puedo lograr", " pero lo voy a lograr"));
+Console.ReadKey(true);
